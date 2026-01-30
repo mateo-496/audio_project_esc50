8a898997367197e45e922affdc41c6199ff86eba
mateo-496 <mateo496@protonmail.com>
2026-01-30 18:04:37 -0500

    Added config file. Added Prediction logic for a single audio file in a dedicated folder

---
+++ src/config/config.py
@@ -0,0 +1,10 @@
+parameters = {
+    "n_bands"  : 128,
+    "n_mels" : 128,
+    "frame_size" : 1024,
+    "hop_size": 1024,
+    "sample_rate": sample_rate,
+    "fft_size": 8192,
+}
+
+sample_rate = 44100
